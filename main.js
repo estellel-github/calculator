d53a877497609cc8b2d65dbb8819048c633f7f81
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-08-03 12:49:26 +0200

    Add several features to JS, make other edits -- "display" -> "screen" -- "/" -> "%" -- adjustFontSize(): Make font size resizeable based on screen content -- deleteLastNumber(): User can delete latest digit from screen -- Remove unnecessary "trim" for button content (not needed after removing unnecessary line breaks from HTML file) -- roundNumber(): Round number result to 7 decimals max -- Add TO DO list of improvements

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const operate = (num1, operator, num2) => {
       return subtract(num1, num2);
     case "x":
       return multiply(num1, num2);
-    case "/":
+    case "÷":
       return divide(num1, num2);
     default:
       return null;
@@ -30,44 +30,81 @@ const operate = (num1, operator, num2) => {
 };
 
 const numButtons = document.querySelectorAll(".number");
-
-const displayEl = document.querySelector("#display");
-
+const screenEl = document.querySelector("#screen");
 const operatorButtons = document.querySelectorAll(".operator");
-
 const equalBtn = document.querySelector("#equal");
-
 const clearBtn = document.querySelector("#clear");
+const deleteBtn = document.querySelector("#delete");
 
-const clearDisplay = () => {
+const updateScreen = (value) => {
+  screenEl.textContent = ("");
+  screenEl.textContent = value;
+  adjustFontSize();
+};
+
+const clearScreen = () => {
   num1 = "";
   num2 = "";
   result = "";
   operator = "";
-  displayEl.textContent = "0";
-  console.log("Display cleared", num1, num2, result, operator);
+  updateScreen("");
+  console.log("Screen cleared", num1, num2, result, operator);
 };
 
-clearDisplay();
+clearScreen();
 
 clearBtn.addEventListener("click", () => {
-  clearDisplay();
+  clearScreen();
 });
 
-const updateDisplay = (value) => {
-  displayEl.textContent = "";
-  displayEl.textContent = value;
-};
+const deleteLastNumber = () => {
+  if (!num1 && !num2) {
+    updateScreen("NOTHING TO DELETE")
+  }
+  if (!num1) {
+    return;
+  }
+  if (num1 && !num2) {
+    num1 = num1.slice(0, -1);
+    updateScreen(num1);
+    console.log("Num1:", num1, "Operator", operator, "Num2:", num2);
+  }
+  if (num2) {
+    num2 = num2.slice(0, -1);
+    updateScreen(num2);
+  }
+}
+
+deleteBtn.addEventListener("click", () => {
+  deleteLastNumber();
+})
+
+function adjustFontSize() {
+  const maxFontSize = 42;
+  const minFontSize = 32;
+  let fontSize = maxFontSize;
+
+  const textLength = screenEl.textContent.length;
+  if (textLength > 8) {
+    fontSize = maxFontSize - (textLength - 10) * 2;
+  }
+
+  if (fontSize < minFontSize) {
+    fontSize = minFontSize;
+  }
+
+  screenEl.style.fontSize = fontSize + 'px';
+}
 
 numButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    let currentDigit = event.target.textContent.trim();
+    let currentDigit = event.target.textContent;
     if (operator === "") {
       num1 += currentDigit;
-      updateDisplay(num1);
+      updateScreen(num1);
     } else {
       num2 += currentDigit;
-      updateDisplay(num2);
+      updateScreen(num2);
     }
     console.log("Num1:", num1, "Operator", operator, "Num2:", num2);
   });
@@ -75,33 +112,66 @@ numButtons.forEach((button) => {
 
 operatorButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    currentOperator = event.target.textContent.trim();
+    currentOperator = event.target.textContent;
     if (num1 != "") {
       operator = currentOperator;
     }
-    updateDisplay(operator);
+    updateScreen(operator);
     console.log("Num1", num1, "Operator:", operator, "Num2", num2);
   });
 });
+
+function roundNumber(number) {
+  return Math.round(number * 10000000) / 10000000
+}
 
 equalBtn.addEventListener("click", () => {
   if (num1 === "" || num2 === "" || operator === "") {
     return;
   }
-  result = operate(Number(num1), operator, Number(num2));
+  result = roundNumber(operate(Number(num1), operator, Number(num2)));
   if (operator === "/" && Number(num2) === 0) {
-    updateDisplay("No can do!");
+    updateScreen("No can do!");
     setTimeout(() => {
-      updateDisplay("Clearing...");
+      updateScreen("Clearing...");
     }, 1000);
     setTimeout(() => {
-      clearDisplay();
+      clearScreen();
     }, 2000);
   } else {
-    updateDisplay(result);
+    updateScreen(result);
   }
   num1 = result;
   num2 = "";
   operator = "";
   console.log("Result:", result);
 });
+
+// TO DO
+// --DONE-- Limit decimal output (round up)
+// --DONE-- Make screen font resizeable (?)
+// --DONE-- Check output for largest numbers
+// --DONE-- Implement Del button functionality
+// Allow for decimal input (only one dot allowed!)
+// Add row on top of screen to show previous inputs
+// Limit input to 9 digits
+// Add keyboard support
+// Add percentage button
+// Add plus/minus button
+// Display calculation history (?)
+
+
+
+// function deleteNumber() {
+//   currentOperationScreen.textContent = currentOperationScreen.textContent
+//     .toString()
+//     .slice(0, -1)
+// }
+
+// function appendPoint() {
+//   if (shouldResetScreen) resetScreen()
+//   if (currentOperationScreen.textContent === '')
+//     currentOperationScreen.textContent = '0'
+//   if (currentOperationScreen.textContent.includes('.')) return
+//   currentOperationScreen.textContent += '.'
+// }
